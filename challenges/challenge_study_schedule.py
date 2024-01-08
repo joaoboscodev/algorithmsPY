d1eb9d667c5c7ecafeefc0cff8db248289b911de
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-08 19:55:08 -0300

    Req 1- Retorne None se em permanence_period houver alguma entrada inválida

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,4 +2,7 @@ def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
     if target_time is None:
         return None
+    for entry in permanence_period:
+        if not all(isinstance(time, int) and time >= 0 for time in entry):
+            return None
     raise NotImplementedError

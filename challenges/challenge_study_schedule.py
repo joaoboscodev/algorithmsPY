428432c19047fd73906cc2ab56482907d60337fc
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-08 20:14:30 -0300

    Req 1- refatorado pra melhorar complexidade e flake8 verificado

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,8 +1,22 @@
+def is_valid_entry(entry):
+    return all(isinstance(time, int) and time >= 0 for time in entry)
+
+
+def count_students_in_target_time(permanence_period, target_time):
+    student_count = 0
+
+    for entry in permanence_period:
+        if entry[0] <= target_time <= entry[1]:
+            student_count += 1
+
+    return student_count
+
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
     if target_time is None:
         return None
-    for entry in permanence_period:
-        if not all(isinstance(time, int) and time >= 0 for time in entry):
-            return None
-    raise NotImplementedError
+
+    if any(not is_valid_entry(entry) for entry in permanence_period):
+        return None
+
+    return count_students_in_target_time(permanence_period, target_time)

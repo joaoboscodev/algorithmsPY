a18f3a2043971de5213864f928f95e798e0a676c
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-08 21:01:12 -0300

    Req 2 - flake8 verificado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,18 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message(111, 2)
+
+    with pytest.raises(TypeError):
+        encrypt_message("Joaobosco", "naoKey")
+
+    assert encrypt_message("Chagas", 3) == "ahC_sag"
+
+    assert encrypt_message("Joao", 2) == "oa_oJ"
+
+    assert encrypt_message("Bosco", 4) == "o_csoB"
+
+    assert encrypt_message("Joao", 4) == "oaoJ"

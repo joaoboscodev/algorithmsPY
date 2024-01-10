b6959aa42ddbe9be06e63dd1186f8b7349ea503d
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-10 18:40:46 -0300

    Req 05 - Flake8 verificado

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,25 @@
+def is_valid_input(nums):
+    return isinstance(nums, list) and nums and all(
+        isinstance(num, int) and num >= 0 for num in nums)
+
+
+def sort_nums(nums):
+    return sorted(nums)
+
+
+def find_duplicate_sorted(nums):
+    for i in range(1, len(nums)):
+        if nums[i] == nums[i - 1]:
+            return nums[i]
+    return False
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not is_valid_input(nums):
+        return False
+
+    if len(nums) == 1:
+        return False
+
+    sorted_nums = sort_nums(nums)
+    return find_duplicate_sorted(sorted_nums)
